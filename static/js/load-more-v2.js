52a851b108d3eaf7c2fd5a4b070abd67d65dc044
Matthew <matthew@buttons.(none)>
2012-02-26 11:42:34 +0000

    tidied up the javascript

--- static/js/load-more-v2.js
+++ static/js/load-more-v2.js
@@ -6,7 +6,8 @@ function LoadMoreRequest (offset, userlat, userlong) {
 	this.offset = offset;
 	this.userlat = userlat;
 	this.userlong = userlong;
-	this.resource = "/ajax/stops/" + this.userlat + "+" + this.userlong + "/" + offset
+	this.resource = "/ajax/stops/" + this.userlong + "+" + this.userlat + "/" + offset
+	parentThis = this;
 }
 
 LoadMoreRequest.prototype.load = function() {
@@ -15,25 +16,22 @@ LoadMoreRequest.prototype.load = function() {
 	l.setAttribute("class", "button loading");
 	// open the request, grab the relevant stops
 	httpRequest=new XMLHttpRequest();
-	httpRequest.open("GET", "/ajax" + this.resource, true);
+	httpRequest.open("GET", this.resource, true);
 	// when received, if it is a 200, create the stops
-	httpRequest.onreadystatechange = function () 
-	{
-	if (httpRequest.readyState === 4) 
-	{
-	  if (httpRequest.status === 200) 
-	  {
-	    this.info = JSON.parse(httpRequest.responseText);
-	    this.createHTML();
-	    // hide the load-more if there aren't any more
-	    if (info.more == false)
-	    {
-	      var rem = document.getElementById('load-more');
-	      rem.style.display = 'none';
-	    }
-	  }
+	httpRequest.onreadystatechange = function () {
+		if (httpRequest.readyState === 4) {
+	  		if (httpRequest.status === 200) {
+	    		parentThis.info = JSON.parse(httpRequest.responseText);
+	    		parentThis.createHTML();
+	    		// hide the load-more if there aren't any more
+	    		if (parentThis.info.more == false) {
+	      			var rem = document.getElementById('load-more');
+	      			rem.style.display = 'none';
+	    		}
+	  		}
+		}
 	}
-	}
+
 	httpRequest.send(null);
 	// increment the offset
 	return offset + 10;
@@ -42,19 +40,19 @@ LoadMoreRequest.prototype.load = function() {
 LoadMoreRequest.prototype.buildFirstItem = function(item, h) {
   
   h += "<section class=\"buses\">";
-  h += "<a class=\"show-more\" title=\"Show\/hide\" onclick=\"toggle_visibility('" + index + "');\">";
+  h += "<a class=\"show-more\" title=\"Show\/hide\" onclick=\"toggle_visibility('" + item + "');\">";
   h += "<div class=\"nextbus\">";
   h += "<div class=\"service\">";
-  h += "<h4>" + obj.buses[item].service + "<\/h4>";
+  h += "<h4>" + this.obj.buses[item].service + "<\/h4>";
   h += "<\/div>";
   h += "<div class=\"businfo\">";
-  h += "<p>To " + obj.buses[0].destination + "<\/p>";
-  h += "<p class=\"time\">" + obj.buses[item].minutes_to_departure + " minutes away<\/p>";
+  h += "<p>To " + this.obj.buses[0].destination + "<\/p>";
+  h += "<p class=\"time\">" + this.obj.buses[item].minutes_to_departure + " minutes away<\/p>";
   h += "<\/div>";
   h += "<h4 class=\"show-more button\">Show more<\/h4>";
   h += "<\/div>";
   h += "<\/a>";
-  h += "<div class=\"bus-list\" id=\"" + index + "\">";
+  h += "<div class=\"bus-list\" id=\"" + item + "\">";
   h += "<h4>Later buses:<\/h4>";
   h += "<ul>";
 
@@ -67,35 +65,35 @@ LoadMoreRequest.prototype.buildStop = function(obj, index) {
 	var h="";
 
 	// enter the basic information about the stop
-	h += "<a class =\"details\" href=\"\/stop\/" + obj.atco + "?userlat=" + userlat + "&amp;userlong=" + userlong + "\" title=\"More about {{ stop.name }}\">";
-	h += "<h2 class=\"stopname\">" + obj.name + "<\/h2>";
+	h += "<a class =\"details\" href=\"\/stop\/" + this.obj.atco + "?userlat=" + this.userlat + "&amp;userlong=" + this.userlong + "\" title=\"More about this.obj.name\">";
+	h += "<h2 class=\"stopname\">" + this.obj.name + "<\/h2>";
 	h += "<div class=\"location\">";
-	h += "<h4>" + obj.distance + " metres away<\/h4>";
+	h += "<h4>" + this.obj.distance + " metres away<\/h4>";
 	h += "<h4 class=\"show-map button\">Show on map<\/h4>";
 	h += "<\/div>";
 	h += "<div class=\"spacer\"><\/div>";
 	h += "<\/a>";
 
 	// if there are some buses, start building their info
-	if (obj.buses.length > 0)
+	if (this.obj.buses.length > 0)
 	{
-	  for (item in obj.buses)
+	  for (item in this.obj.buses)
 	  {
 	    // if it is the first bus, build it out as the first bus and start the list of later buses
 	    if (item == 0)
 	    {
-	      buildFirstItem(item)
+	      h = this.buildFirstItem(item, h)
 	    }
 	    // for the other buses, build their info
 	    else
 	    {
 	      h += "<li class=\"bus\">";
 	      h += "<div class=\"service\">";
-	      h += "<h4>" + obj.buses[item].service + "<\/h4>";
+	      h += "<h4>" + this.obj.buses[item].service + "<\/h4>";
 	      h += "<\/div>";
 	      h += "<div class=\"businfo\">";
-	      h += "<p>To " + obj.buses[item].destination + "<\/p>";
-	      h += "<p class=\"time\">" + obj.buses[item].minutes_to_departure + " minutes away<\/p>";
+	      h += "<p>To " + this.obj.buses[item].destination + "<\/p>";
+	      h += "<p class=\"time\">" + this.obj.buses[item].minutes_to_departure + " minutes away<\/p>";
 	      h += "<\/div>";
 	      h += "<\/li>";
 	    }
@@ -128,8 +126,8 @@ LoadMoreRequest.prototype.createHTML = function() {
   // for all of the stops in the list, create a stop using the create stop process designed above
   for (i in this.info.stops) 
   {
-    var obj = this.info.stops[i];       
-    this.buildStop(obj, i+this.offset);
+    this.obj = this.info.stops[i];       
+    this.buildStop(this.obj, i+this.offset);
   }
 
   // hide all the buses for the loaded stops
@@ -140,4 +138,10 @@ LoadMoreRequest.prototype.createHTML = function() {
   }
   // hide the loading gif
   l.setAttribute("class", "button");
+}
+
+function doLoad (offset, userlong, userlat) {
+	loadrequest = new LoadMoreRequest (offset, userlat, userlong);
+	offset = loadrequest.load();
+	return offset;
 }
