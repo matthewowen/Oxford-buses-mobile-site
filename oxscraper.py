58a61b8b81f2d60f59c759b68e9d7350e06a7cf8
Matthew Owen <matthew.owen95@gmail.com>
2012-07-22 11:54:50 +0100

    better fontpage, more consistency on bus times

--- oxscraper.py
+++ oxscraper.py
@@ -2,55 +2,57 @@ import requests
 from BeautifulSoup import BeautifulSoup
 
 def get_content(URL):
-	"""
-	Pass a URL. Get back some Beautiful Soup.
-	"""
+    """
+    Pass a URL. Get back some Beautiful Soup.
+    """
 
-	r = requests.get(URL)
-	soup = BeautifulSoup(r.text)
+    r = requests.get(URL)
+    soup = BeautifulSoup(r.text)
 
-	return soup
+    return soup
 
 def parse_soup(soup):
-	"""
-	parses the soup we get from get_buses
-	"""
+    """
+    parses the soup we get from get_buses
+    """
 
-	stopTable = soup.findAll('table')[1]
-	stopBody = stopTable.find('tbody')
-	if not stopBody:
-		return []
+    stopTable = soup.findAll('table')[1]
+    stopBody = stopTable.find('tbody')
+    if not stopBody:
+        return []
 
-	rows = stopBody.findAll('tr')
+    rows = stopBody.findAll('tr')
 
-	bus_list = []
+    bus_list = []
 
-	for row in rows:
-		d = {}
-		cells = row.findAll('td')
+    for row in rows:
+        d = {}
+        cells = row.findAll('td')
 
-		d['service'] = cells[0].string
-		d['destination'] = cells[1].string.replace('&amp;', '&')
-		d['minutes_to_departure'] = cells[2].string.replace(' mins', '')
+        d['service'] = cells[0].string
+        d['destination'] = cells[1].string.replace('&amp;', '&')
+        d['minutes_to_departure'] = cells[2].string.replace(' mins', '')
+        if d['minutes_to_departure'] == 'DUE':
+            d['minutes_to_departure'] = 0
 
-		bus_list.append(d)
+        bus_list.append(d)
 
-	return bus_list
+    return bus_list
 
 class stop(object):
-	"""
-	init with a stop id
-	get back buses for that stop as a list of dictionaries
-	"""
+    """
+    init with a stop id
+    get back buses for that stop as a list of dictionaries
+    """
 
-	def get_buses(self):
-		url = 'http://www.oxontime.com/Naptan.aspx?t=departure&sa=%s&format=xhtml' % (self.id)
+    def get_buses(self):
+        url = 'http://www.oxontime.com/Naptan.aspx?t=departure&sa=%s&format=xhtml' % (self.id)
 
-		soup = get_content(url)
+        soup = get_content(url)
 
-		self.bus_list = parse_soup(soup)
+        self.bus_list = parse_soup(soup)
 
-	def __init__(self, id):
-		self.id = id
+    def __init__(self, id):
+        self.id = id
 
-		self.get_buses()
+        self.get_buses()
